59b72abaca32e5d3ba8dee0840985e04a9539a12
TomMahe <mahettom@gmail.com>
2023-01-30 09:52:52 +0100

    almost done

--- src/index.js
+++ src/index.js
@@ -1,23 +1,28 @@
 // ITERATION 1
-
+let result = 0;
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  const price = product.querySelector('.price span').textContent;
+  const quantity = product.querySelector('.quantity input').value;
+  let subtotal = product.querySelector('.subtotal span');
 
-  //... your code goes here
+  result = price * quantity;
+  subtotal.textContent = price * quantity;
+  //console.log(result)
+  return result;
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
+  let cardTotal = 0;
+  let totalValue = document.querySelector('#total-value span');
+  const allProduct = document.querySelectorAll('.product');
 
-  // ITERATION 2
-  //... your code goes here
-
-  // ITERATION 3
-  //... your code goes here
+  allProduct.forEach(oneProduct => {
+    updateSubtotal(oneProduct)
+    cardTotal += result;
+    // console.log('result =', result)
+    // console.log('cardtotal =', cardTotal)
+    totalValue.textContent = cardTotal;
+  });
 }
 
 // ITERATION 4
@@ -29,9 +34,35 @@ function removeProduct(event) {
 }
 
 // ITERATION 5
+const createButton = document.querySelector('#create.btn');
+
+createButton.addEventListener('click', createProduct())
 
 function createProduct() {
-  //... your code goes here
+  const newName = document.querySelector('#newProductName').textContent;
+  const newPrice = document.querySelector('#newProductPrice').value;
+  const cartTable = document.querySelector('tbody');
+
+  const newRow = cartTable.createElement('tr')
+  newRow.className = 'product'
+  const newProductName = newRow.createElement('name')
+  const newProductPrice = newRow.createElement('price')
+
+
+
+  //const newDiv = document.createElement("div");
+
+  // and give it some content
+  //const newContent = document.createTextNode("Hi there and greetings!");
+
+  // add the text node to the newly created div
+  //newDiv.appendChild(newContent);
+
+  // add the newly created element and its content into the DOM
+  //const currentDiv = document.getElementById("div1");
+  //document.body.insertBefore(newDiv, currentDiv);
+
+
 }
 
 window.addEventListener('load', () => {
